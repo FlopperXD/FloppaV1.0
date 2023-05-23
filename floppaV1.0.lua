4fdd64ecffd1db376ca383582973f9ecf161c40e
FlopperXD <134342068+FlopperXD@users.noreply.github.com>
2023-05-23 13:56:43 -0500

    Create floppaV1.0.lua

---
+++ floppaV1.0.lua
@@ -0,0 +1,13 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Floppa HUB 1.0", "GrapeTheme")
+
+local Tab = Window:NewTab("Bedwars")
+local Section = Tab:NewSection("More Support More Scripts")
+
+Section:NewButton("vape (need config)", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/7GrandDadPGN/VapeV4ForRoblox/main/NewMainScript.lua", true))()
+end)
+
+Section:NewButton("Keyboard", "ButtonInfo", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/advxzivhsjjdhxhsidifvsh/mobkeyboard/main/main.txt", true))()
+end)
